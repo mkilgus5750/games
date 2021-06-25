7e6572a8703060b601dd3b1037153d6fc8215fdf
Mari Kilgus <60790112+mkilgus5750@users.noreply.github.com>
2021-06-25 15:52:11 -0400

    added game detail page with round information

--- game_keeper/views.py
+++ game_keeper/views.py
@@ -12,6 +12,15 @@ def index(request):
 def games(request):
     return render(request, 'game_keeper/all-games.html')
 
-# def game_detail(request, slug):
-# display round details, winner of each round, winner of each game
-# will have to implement round page and game_detail page
+def game_detail(request, slug):
+    game = get_object_or_404(Game, slug=slug)
+    all_rounds = game.rounds.all()
+    rounds = tuple(round for round in all_rounds),
+    players = tuple(round.players.all() for round in all_rounds)
+    return render(request, 'game_keeper/game-detail.html', {
+        'game': game,
+        'rounds': rounds,
+        'players': players
+    })
+    # display round details, winner of each round, winner of each game
+    # will have to implement round page and game_detail page
